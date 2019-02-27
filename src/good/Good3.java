e6a86cc927b499b38ab72049f4182b420bde9c5a
shreyaU09 <ugavekarshreya@yahoo.in>
2019-02-27 07:04:54 +0530

    one commit

--- src/good/Good3.java
+++ src/good/Good3.java
@@ -5,6 +5,7 @@ public class Good3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is fine");
+System.out.println("This is fine one");
 	}
 
 }
